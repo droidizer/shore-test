1b0c9cef831a1d635a07885eb71a6396e2ca3770
Shane Murphy <shane.murphy@shore.com>
2016-04-14 12:09:00 +0200

    Updated the read me text

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # AndroidInterviewApp
-Base framwork of application to implement programming challenges
 
-Currently it has two tasks
-1. Bug fix
-2. Implement feature
+Challenge
+
+You have one hour to do as much of this challenge as possible. You ARE allowed to use the internet.
+You CAN contact the interviewer to clarify anything during this challenge.
+
+There are 3 tasks:
+
+1. Bug fix  - Details and Edit Activity
+
+When you edit the data (Edit Activity) it is not reflected when your result is passed back to the
+Details Activity. Fix this bug.
+
+2. Implement feature  - Feature Activity
+
+ListView is badly implemented. It could be improved by using the ViewHolder design pattern.
+Please refactor this feature to use the ViewHolder design pattern.
+
+
+3. Write tests for the following class - Contact in ContactTest class
+
+Please write some tests for the Contact class. Class should have 100% code coverage.
+
+
+
--- app/src/androidTest/java/termine24/secondinterviewtest/app/model/ContactTest.java
+++ app/src/androidTest/java/termine24/secondinterviewtest/app/model/ContactTest.java
@@ -7,30 +7,10 @@ package termine24.secondinterviewtest.app.model;
  */
 public class ContactTest extends ParcelableTest {
 
-    private String name;
-    private String email;
-    private String phoneNumber;
-    private Contact contact;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        name = "testguy";
-        email = "testguy@shore.com";
-        phoneNumber = "01234567890";
-        contact = new Contact(name, email, phoneNumber);
-    }
-
-    public void testEmptyConstructor() throws Exception {
-        Contact emptyContact = new Contact();
-        assertNotNull("Empty constructor returning null", emptyContact);
-    }
-
-    public void testConstructor() throws Exception {
-        assertNotNull("Parameter constructor is returning null", contact);
-        assertEquals("Name is not the same as the constructor received", name, contact.getName());
-        assertEquals("Email is not the same as the constructor received", email, contact.getEmail());
-        assertEquals("PhoneNumber is not the same as the constructor received", phoneNumber, contact.getMobile());
     }
 
     //TODO implement test for 100% branch and function coverage
